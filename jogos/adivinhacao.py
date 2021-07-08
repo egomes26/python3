7e00ffef077d1ee3c53c4a79be0fd97130601d20
Ericson Gomes <ericson.gomes.26@gmail.com>
2021-07-08 15:03:03 -0300

    04-A Sequencia do jogo

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -3,14 +3,29 @@ print("Bem vindo ao Jogo de adivinhação")
 print("*********************************")
 
 numero_secreto =42
+total_de_tentativas = 3
+rodada = 1
 
-chute = int(input("Digite seu numero: "))
 
-print("Você digitou ", chute)
+while (rodada <=total_de_tentativas):
 
-if(numero_secreto == chute):
-    print("Acertou Miseravi")
-else:
-    print("Errrrrrou")
+    print("Tentativa {} de {}".format(rodada,total_de_tentativas))
+    chute = int(input("Digite seu numero: "))
+
+    print("Você digitou ", chute)
+
+    acertou = numero_secreto == chute
+    chute_maior = chute > numero_secreto
+    chute_menor = chute < numero_secreto
+
+    if acertou:
+        print("Acertou Miseravi")
+    else:
+        if chute_maior:
+            print("Errrrrrou! chute maior que numero secreto!")
+        elif chute_menor:
+            print("Errrrrrou! chute menor que numero secreto!")
+
+    rodada += 1
 
 print("End Game")
